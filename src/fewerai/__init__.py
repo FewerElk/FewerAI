c596f1ed5ede5f604a4852ba871c7ab0b081c97a
FewerElk <120582916+FewerElk@users.noreply.github.com>
2024-04-09 11:48:02 +0200

    patch

--- src/fewerai/__init__.py
+++ src/fewerai/__init__.py
@@ -11,6 +11,9 @@ class BadRequestException(Exception):
     """Raised when the server can't decode the request."""
     pass
 
+class ServiceUnavailableException(Exception):
+    """Raised when the server get an internall exceptio, or when the server is not reachable"""
+
 class API(object):
     def __init__(self, username:str, token:str):
         self.username = username
@@ -18,18 +21,21 @@ class API(object):
         
     def generate(self, request:str):
         """Uses the FewerAI API to generate a text"""
-        # Formatage des données dans le format spécifié
-        data = "{}%{}%{}".format(self.username, self.token, request)
+        try:
+            data = "{}%{}%{}".format(self.username, self.token, request)
 
-        # URL de l'API Flask
-        url = "http://n1.recloud-hosting.me:1123/api"
+            # URL de l'API Flask
+            url = "http://n1.recloud-hosting.me:1123/api"
 
-        # Envoi de la requête POST avec les données au serveur
-        response = requests.post(url, data=data)
+            response = requests.post(url, data=data)
 
-        if response.text == "400":
-            raise BadRequestException("Bad Request. Please update the version of this client or post an issue here : https://github.com/FewerElk/FewerAI/issues")
-        elif response.text == "403":
-            raise LoginException("Failed to login: incorrect username or password.")
-        # Affichage de la réponse du serveur
-        return response.text
+            if response.text == "400":
+                raise BadRequestException("Bad Request. Please update the version of this client or post an issue here : https://github.com/FewerElk/FewerAI/issues")
+            elif response.text == "403":
+                raise LoginException("Failed to login: incorrect username or password.")
+            elif response.text == "503":
+                raise ServiceUnavailableException("An internal exception occured when performing the generation. Maybe the server couldn't decode your request. Did you used utf-8 caracters ?")
+            else:
+                return response.text
+        except requests.exceptions.ConnectionError:
+            raise ServiceUnavailableException("Could not connect to the server. Please open an issue.")
